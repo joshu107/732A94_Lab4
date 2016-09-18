d9a5bb00eed66cbeab150807365783aff2cca928
nickto <nickto0x2c@gmail.com>
2016-09-18 17:52:04 +0200

    Add test for missing methods

--- tests/testthat/test_linreg.R
+++ tests/testthat/test_linreg.R
@@ -6,12 +6,13 @@ test_that("Wrong parameters", {
   expect_error(linreg("formula", function(x) {}), "wrong parameters")
   expect_error(linreg(X ~ Sepal.Width, data = iris), "variable(s) not in data",
                fixed = TRUE)
-  expect_error(linreg(Sepal.Width ~ ., data = iris), "variable(s) not in data")
+  expect_error(linreg(Sepal.Width ~ ., data = iris), "variable(s) not in data",
+               fixed = TRUE)
   expect_error(linreg(x ~ y, data.frame(x = numeric(0), y = numeric(0))),
                "data must have >=1 cols/rows")
 })
 
-context("Testing methods of LinregClass")
+context("Testing methods of Linreg class")
 
 # Compare results to lm function
 lmObject <-         lm(formula = mpg ~ hp, data = mtcars)
@@ -20,9 +21,13 @@ test_that("Same as in lm", {
   expect_that(linregObject$coef(),  equals(lmObject$coefficients))
   expect_that(linregObject$resid(), equals(lmObject$residuals))
   expect_that(linregObject$pred(),  equals(predict(lmObject)))
+  expect_that(linregObject$print(),  equals(NULL))
+  expect_that(linregObject$plot(),  equals(NULL))
+  expect_that(linregObject$summary(),  equals(NULL))
 })
 
 # Test caching
+context("Testing caching of Linreg class")
 lmObject <-         lm(formula = mpg ~ hp, data = mtcars)
 linregObject <- linreg(formula = mpg ~ hp, data = mtcars)
 
