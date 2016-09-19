5f0944dc72a833dc58515cc1a29fca16f0002581
torda385 <torda385@student.liu.se>
2016-09-19 11:21:53 +0200

    Updated qrlinreg.  Consider ways to replace normal regression method that is used in linreg

--- R/qrlinreg.R
+++ R/qrlinreg.R
@@ -8,7 +8,7 @@
 #' @param data a data frame or (or object coercible by
 #'   \code{\link{as.data.frame}}).
 #'
-#' @return THIS NEEDS TO BE CHANGED \code{qrlinreg} returns an
+#' @return \code{qrlinreg} returns an
 #'  object of class "\code{Linreg}".
 #'
 #'   An object of class "\code{Linreg}" have the following methods:
@@ -16,7 +16,7 @@
 #'   \item{residuals()}{the residuals, that is response minus fitted values}
 #'   \item{pred()}{predicted values of of a model}
 #'
-#' @seealso \code{\link{lm}}, \code{\link{class}}, \code{\link{formula}}
+#' @seealso \code{\link{qr}}, \code{\link{class}}, \code{\link{formula}}
 #'
 #' @examples
 #' qrlinreg(formula = Sepal.Length ~ Sepal.Width, data = iris)
@@ -27,46 +27,21 @@
 #' @export
 
 
-qrlinreg <- function(){
-  
+qrlinreg<-function(x,y,...){
+  x<-unclass(x)
+  y<-unclass(y)
+  X<-cbind(1,x)
+  n<-nrow(X)
+  p<-ncol(X)
+  qr.X<-qr(X)
+  b<-(t(qr.Q(qr.X)) %*% y)[1:p]
+  R<-qr.R(qr.X)
+  beta<-as.vector(backsolve(R,b))
+  res<-as.vector(y-X %*% beta)
+  beta.names <- paste("X", 1:(p-1), sep = "")
+  beta.names <- c("Intercept", beta.names)
+  names(beta)<-beta.names
+  ans <- list(coef = beta, residuals = res, intercept = TRUE)
+  return(ans)
 }
-
-
-#args(lsfit)
-
-#set.seed(1)
-
-#x<-rnorm(5)
-#y<-rnorm(5)
-
-#fit<-lsfit(x,y)
-
-#if(fit$intercept){
-#  X<-cbind(1,x)
-#} else{
-#  X<-x
-#}
-
-#n<-nrow(X)
-#p<-ncol(X)
-
-#qr.X<-qr(X)
-
-#b<-(t(qr.Q(qr.X)) %*% y)[1:p]
-
-#R<-qr.R(qr.X)
-#beta<-as.vector(backsolve(R,b))
-
-#res<-as.vector(y-X %*% beta)
-
-#if(fit$intercept) {
-#  beta.names <- paste("X", 1:(p-1), sep = "")
-#  beta.names <- c("Intercept", beta.names)
-#} else {
-#  beta.names <- paste("X", 1:p, sep = "")
-#}
-#names(beta)<-beta.names
-#ans <- list(coef = beta, residuals = res, intercept = fit$intercept)
-#var.ans<-var(res)
-
 
